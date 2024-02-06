0233818c32bb347dacd481b76a2f12effd6f0f49
unknown <lmechegaray@wecom.global>
2024-02-06 17:47:13 -0300

    changes

--- controllers/facturacionbd.js
+++ controllers/facturacionbd.js
@@ -4,8 +4,6 @@ const Actividad = require("../models/actividad.js")
 const Periodos = require("../models/periodos.js")
 const sequelize = require("../config/db.js");
 
-
-
 async function saveFacturacion(dataClient) {
  
 
